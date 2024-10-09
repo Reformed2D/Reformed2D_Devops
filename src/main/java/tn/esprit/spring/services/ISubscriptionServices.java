c7c9f6242fae4c9706146e70fd4578eff353654c
mohamedhadji1 <100348710+mohamedhadji1@users.noreply.github.com>
2024-10-09 14:50:22 -0700

    Update ISubscriptionServices.java

--- src/main/java/tn/esprit/spring/services/ISubscriptionServices.java
+++ src/main/java/tn/esprit/spring/services/ISubscriptionServices.java
@@ -10,14 +10,9 @@ import tn.esprit.spring.entities.TypeSubscription;
 public interface ISubscriptionServices {
 
 	Subscription addSubscription(Subscription subscription);
-
 	Subscription updateSubscription(Subscription subscription);
-
 	Subscription retrieveSubscriptionById(Long numSubscription);
-
 	Set<Subscription> getSubscriptionByType(TypeSubscription type);
-
 	List<Subscription> retrieveSubscriptionsByDates(LocalDate startDate, LocalDate endDate);
-
 	void retrieveSubscriptions();
 }
